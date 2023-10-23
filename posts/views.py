36fd6eb40a042fafd9568239f9cadbbcb5fb5e51
Hyunjun Kim <68070764+hyunjun33@users.noreply.github.com>
2023-10-23 23:18:50 +0900

    feed pages built
    Post ,PostImage, Comment model built
    Admin built
    Feed view built
    Feed page template built

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
+from posts.models import Post
 
 # Create your views here.
 def feeds(request):
     if not request.user.is_authenticated:
         return redirect("/users/login")
 
+    # 모든 글 목록을 템플릿으로 전달
+    posts = Post.objects.all()
+    context = {"posts": posts}
 
 
-    return render(request, "posts/feeds.html")
+    return render(request, "posts/feeds.html", context)
