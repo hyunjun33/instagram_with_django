6e0b567c0aeff3ef12507bbf5c411afb260dc687
Hyunjun Kim <68070764+hyunjun33@users.noreply.github.com>
2023-10-25 23:58:01 +0900

    CommentForm function built
    CommentForm function codes added

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from posts.models import Post
+from posts.forms import CommentForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def feeds(request):
@@ -8,7 +10,33 @@ def feeds(request):
 
     # 모든 글 목록을 템플릿으로 전달
     posts = Post.objects.all()
-    context = {"posts": posts}
+    comment_form = CommentForm()
 
+    context = {"posts": posts,
+               "comment_form": comment_form,
+
+               }
 
     return render(request, "posts/feeds.html", context)
+
+@require_POST
+def comment_add(request):
+    # request.POST로 전달된 데이터를 사용해 CommentForm 인스턴스를 생성
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # commit=False 옵션으로 메모리상에 Comment 객체 생성
+        comment = form.save(commit=False)
+
+        # Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
+        comment.user = request.user
+
+        # DB에 Comment 객체 저장
+        comment.save()
+
+        # 생성된 Comment의 정보 확인
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+
+        # 생성 완료 후에는 피드 페이지로 다시 이동
+        return redirect("/posts/feeds/")
